34074b41e5d70aec233535fcd4d3bed3d973d754
davidvaini <davidvaini@gmail.com>
2016-03-31 11:40:31 -0500

    completed type conversion  exercise

--- topics/type_conversions/exercises/template1/template1.go
+++ topics/type_conversions/exercises/template1/template1.go
@@ -13,26 +13,33 @@
 // of i to c. Does the compiler allow the assignment?
 package main
 
-// Add imports.
+import "fmt"
 
-// Declare the named type called counter with a base
-// type of int.
+// counter is a named type for counting.
+type counter int
 
 // main is the entry point for the application.
 func main() {
-	// Declare a variable named c of type counter
-	// set to its zero value.
+	// Declare a variable of type counter.
+	var c counter
 
 	// Display the value of c.
+	fmt.Println(c)
 
-	// Declare a variable named c2 of type counter
-	// set to the value of 10.
+	// Declare a second variable of type counter. Set
+	// the value to 10.
+	c2 := counter(10)
 
 	// Display the value of c2.
+	fmt.Println(c2)
 
-	// Declare a variable named i of the base type int.
+	// Declare a variable named i of type int.
+	i := 1
 
 	// Assign the value of i to the variable named c.
+	c = i
 
-	// Does it compile?
+	// Does the program compile?
+	fmt.Println(c)
+	// no it does not.
 }
