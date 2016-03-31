e4ed22ee1e3673b82f9df376b3067741c99aa11f
davidvaini <davidvaini@gmail.com>
2016-03-31 10:56:53 -0500

    completed pointer exercise

--- topics/pointers/exercises/template1/template1.go
+++ topics/pointers/exercises/template1/template1.go
@@ -11,17 +11,21 @@
 // _value that the pointer points to_.
 package main
 
-// Add imports.
+import "fmt"
 
 // main is the entry point for the application.
 func main() {
 	// Declare an integer variable with the value of 20.
+	value := 20
 
 	// Display the address of and value of the variable.
+	fmt.Println("Address Of:", &value, "Value Of:", value)
 
 	// Declare a pointer variable of type int. Assign the
 	// address of the integer variable above.
+	p := &value
 
 	// Display the address of, value of and the value the pointer
 	// points to.
+	fmt.Println("Address Of:", &p, "Value Of:", p, "Points To:", *p)
 }
